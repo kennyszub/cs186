35ebacbfef914e10316b66521ebf64e0d0924872
saasbook <saasbook@saasbook.(none)>
2012-12-01 19:50:04 -0800

    part 1 done without testing

--- hw5/postgres-8.4.2/src/backend/utils/hash/cmsketch.c
+++ hw5/postgres-8.4.2/src/backend/utils/hash/cmsketch.c
@@ -10,7 +10,15 @@
 
 /* initialize the count-min sketch for the specified width and depth */
 cmsketch* init_sketch(uint32 width, uint32 depth) {
-  return NULL;
+  cmsketch *sketch = (cmsketch *) palloc0(sizeof(cmsketch));
+  sketch->cmdata = palloc0((width * depth) * sizeof(uint32));
+  if (sketch->cmdata == NULL) {
+    fprintf(stderr, "out of memory\n");
+    return NULL;
+  }
+  sketch->cmdataWidth = width;
+  sketch->cmdataDetph = depth;
+  return sketch;
 }
 
 /* increment 'bits' in each sketch by 1. 
@@ -18,7 +26,14 @@ cmsketch* init_sketch(uint32 width, uint32 depth) {
  *    Thus, each index is between 0 and 'width', and there are 'depth' of them.
  */
 void increment_bits(cmsketch* sketch, uint32 *bits) {
-
+  uint32 row;
+  uint32 column;
+  uint32 width = sketch->cmdataWidth;
+  uint32 depth = sketch->cmdataDepth;
+  for (row = 0; row < depth; row++) {
+    column = bits[row];
+    sketch->cmdata[width * row + column] += 1;
+  }
 }
 
 /* decrement 'bits' in each sketch by 1.
@@ -26,7 +41,16 @@ void increment_bits(cmsketch* sketch, uint32 *bits) {
  *    Thus, each index is between 0 and 'width', and there are 'depth' of them.
  */
 void decrement_bits(cmsketch* sketch, uint32 *bits) {
-
+  uint32 row;
+  uint32 column;
+  uint32 width = sketch->cmdataWidth;
+  uint32 depth = sketch->cmdataDepth;
+  for (row = 0; row < depth; row++) {
+    column = bits[row];
+    if (sketch->cmdata[width * row + column] != 0) { 
+      sketch->cmdata[width * row + column] -= 1;
+    }
+  }
 }
 
 /* return the minimum among the indicies pointed to by 'bits'
@@ -34,15 +58,32 @@ void decrement_bits(cmsketch* sketch, uint32 *bits) {
  *    Thus, each index is between 0 and 'width', and there are 'depth' of them.
  */
 uint32 estimate(cmsketch* sketch, uint32 *bits) {
-  return 0;
+  uint32 row;
+  uint32 column;
+  uint32 width = sketch->cmdataWidth;
+  uint32 depth = sketch->cmdataDepth;
+  uint32 minimum = UINT_MAX;
+  uint32 current;
+  for (row = 0; row < depth; row++) {
+    column = bits[row];
+    current = sketch->cmdata[width * row + column];
+    if (current < minimum) {
+      minimum = current;
+    }
+  }
+  return minimum;
 }
 
 /* set all values in the sketch to zero */
 void reset_sketch(cmsketch* sketch) {
-
+  uint32 sizeOfSketch = sketch->cmdataWidth * sketch->cmdataDepth;
+  uint32 i;
+  for (i = 0; i < sizeOfSketch; i++) {
+    sketch->cmdata[i] = 0;
+  }
 }
 
 /* destroy the sketch, freeing any memory it might be using */
 void destroy_sketch(cmsketch* sketch) {
-
+  pfree(sketch);
 }
